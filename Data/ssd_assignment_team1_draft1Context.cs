f09a507370d630f3dd9fb5ee54dd90d2ae4d5ac4
Kai Zer Chia <s10194854@connect.np.edu.sg>
2020-07-15 11:54:55 +0800

    nothing much

--- Data/ssd_assignment_team1_draft1Context.cs
+++ Data/ssd_assignment_team1_draft1Context.cs
@@ -7,7 +7,7 @@ using ssd_assignment_team1_draft1.Models;
 
 namespace ssd_assignment_team1_draft1.Data
 {
-    public class ssd_assignment_team1_draft1Context : DbContext
+    public class ssd_assignment_team1_draft1Context : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
         public ssd_assignment_team1_draft1Context (DbContextOptions<ssd_assignment_team1_draft1Context> options)
             : base(options)
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ssd_assignment_team1_draft1.Models
+{
+    public class ApplicationUser
+    {
+    }
+}
