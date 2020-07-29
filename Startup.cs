b5685e900142b897ec2b12d8d9db1b0c78e2a7bf
Chin Tiong Ryan Ng <s10196904@connect.np.edu.sg>
2020-07-29 11:53:30 +0800

    Done Google External Login

--- Startup.cs
+++ Startup.cs
@@ -68,7 +68,15 @@ namespace ssd_assignment_team1_draft1
                 options.User.RequireUniqueEmail = true;
             });
 
+            services.AddAuthentication()
+        .AddGoogle(options =>
+        {
+            IConfigurationSection googleAuthNSection =
+                Configuration.GetSection("Authentication:Google");
 
+            options.ClientId = googleAuthNSection["ClientId"];
+            options.ClientSecret = googleAuthNSection["ClientSecret"];
+        });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
