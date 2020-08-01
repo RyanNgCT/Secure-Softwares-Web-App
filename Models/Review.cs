1ab06079c27018963f149c81c948cc90c1b6eaed
Ng Ray Son <s10196601@connect.np.edu.sg>
2020-08-01 22:40:22 +0800

    Review.cs ratings added decimal thing

--- Models/Review.cs
+++ Models/Review.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ssd_assignment_team1_draft1.Models
 {
@@ -11,6 +13,7 @@ namespace ssd_assignment_team1_draft1.Models
         public string SoftwareName { get; set; }
         public string Username { get; set; }
         public DateTime DateCreated { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Ratings { get; set; }
         public string Content { get; set; }
     }
