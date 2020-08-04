4c9a7b4a2cb1f84457788088fdfb487ce9f827d7
Chin Tiong Ryan Ng <s10196904@connect.np.edu.sg>
2020-08-04 18:23:00 +0800

    Input validation for change password (when user is alr logged in) and Reset Password (via email)

--- Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -33,10 +33,10 @@ namespace ssd_assignment_team1_draft1.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
             [DataType(DataType.Password)]
             public string Password { get; set; }
-
+            [Required]
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
