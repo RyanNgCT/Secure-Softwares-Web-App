dfebec65dc84a3d89da40e91016bfb5a740683e6
Chin Tiong Ryan Ng <s10196904@connect.np.edu.sg>
2020-08-04 18:32:05 +0800

    tidy up startup.cs

--- Startup.cs
+++ Startup.cs
@@ -66,7 +66,6 @@ namespace ssd_assignment_team1_draft1
 
                 // User settings
                 options.User.RequireUniqueEmail = true;
-                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
             });
             services.ConfigureApplicationCookie(options =>
             {
@@ -77,7 +76,7 @@ namespace ssd_assignment_team1_draft1
                 // options.AccessDeniedPath = "/Account/AccessDenied";
 
                 options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromSeconds(300);
+                options.ExpireTimeSpan = TimeSpan.FromSeconds(300); // 5 mins lockout
                 options.SlidingExpiration = true;
 
             });
