f714326a3441857c46952ff48ff638e2a0b222f8
Chin Tiong Ryan Ng <s10196904@connect.np.edu.sg>
2020-08-04 18:26:22 +0800

    Regex for resetpassword email field

--- Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -30,6 +30,8 @@ namespace ssd_assignment_team1_draft1.Areas.Identity.Pages.Account
         {
             [Required]
             [EmailAddress]
+            [RegularExpression(@"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$", ErrorMessage = "Please enter valid email.")]
             public string Email { get; set; }
 
             [Required]
